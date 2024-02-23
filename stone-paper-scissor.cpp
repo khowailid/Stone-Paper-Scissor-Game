5f16697a7dae505f6fe1e5a161433a92d803f09c
unknown <Khowailid@egypt>
2024-02-23 20:51:07 +0200

    Game Project: stone-paper-scissor, Fundementals.

--- stone-paper-scissor.cpp
+++ stone-paper-scissor.cpp
@@ -94,36 +94,97 @@ enWinner WhoWonTheGame(short plyer, short computer) {
 		return enWinner::draw;
 }
 stGameResults FillGameResults(int gameRounds, short player, short computer, short draw) {
-
+	stGameResults GameResults;
+	GameResults.GameRounds = gameRounds;
+	GameResults.Player1WinTimes = player;  
+	GameResults.Computer2WinTimes = computer;     
+	GameResults.DrawTimes = draw;     
+	GameResults.GameWinner = WhoWonTheGame(player, computer);    
+	GameResults.WinnerName = WinnerName(GameResults.GameWinner);
+	return GameResults;
 }
 enGameChoice ReadPlayerChoice(){
-
+	short Choice = 1;
+	do{
+		cout << "\nYour Choice: [1]:Stone, [2]:Paper, [3]:Scissors ? ";      
+		cin >> Choice; 
+	} 
+	while (Choice < 1 || Choice >3); 
+	return (enGameChoice)Choice;
 }
 enGameChoice GetComputerChoice() {
-
+	return  (enGameChoice)randomNumber(1, 3);
 }
 stGameResults playGame(short Rounds) {
-
+	stRoundInfo RoundInfo;
+	short Player1WinTimes = 0, ComputerWinTimes = 0, DrawTimes = 0;
+	for (short GameRound = 1; GameRound <= Rounds; GameRound++) {
+		cout << "\nRound [" << GameRound << "] begins:\n";
+		RoundInfo.RoundNumber = GameRound;
+		RoundInfo.Player1Choice = ReadPlayerChoice();
+		RoundInfo.ComputerChoice = GetComputerChoice();
+		RoundInfo.Winner = WhoWinTheRound(RoundInfo);
+		RoundInfo.WinnerName = WinnerName(RoundInfo.Winner); //Increase win/Draw counters
+		if (RoundInfo.Winner == enWinner::player)             
+				Player1WinTimes++;
+		else if (RoundInfo.Winner == enWinner::computer)             
+				ComputerWinTimes++; 
+		else            
+				DrawTimes++;
+		PrintRoundResults(RoundInfo);    
+	} 
+		return FillGameResults(Rounds, Player1WinTimes, ComputerWinTimes, DrawTimes);
 }
-string Tabs(short numOfTabs) {
 
+string Tabs(short numOfTabs) {
+	string t = ""; 
+	for (int i = 1; i < numOfTabs; i++){
+			t = t + "\t";        
+			cout << t;
+	}
+	return t;
 }
 void showGameOverScrean() {
-
+	cout << Tabs(3) << "_____________________\n\n";
+	cout << Tabs(5) << "+++ Game Over +++\n";
+	cout << Tabs(3) << "_____________________\n\n";
 }
 void ShowFinalGameResults(stGameResults GameResults) {
-
+	cout << Tabs(2) << "_____________________ [Game Results ]_____________________\n\n";     
+	cout << Tabs(2) << "Game Rounds        : " << GameResults.GameRounds<< endl;
+	cout << Tabs(2) << "Player1 won times  : " << GameResults.Player1WinTimes << endl; 
+	cout << Tabs(2) << "Computer won times : " << GameResults.Computer2WinTimes << endl;     
+	cout << Tabs(2) << "Draw times         : " << GameResults.DrawTimes << endl; 
+	cout << Tabs(2) << "Final Winner       : " << GameResults.WinnerName << endl;     
+	cout << Tabs(2) << "___________________________________________________________\n";  
+	setWinnerScreenColor(GameResults.GameWinner);
 }
 short ReadHowManyRounds() {
-	do
+	short GameRounds = 1;
+	do { 
+		cout << "How Many Rounds 1 to 10 ? \n";  
+	cin >> GameRounds;
+	} while (GameRounds < 1 || GameRounds >10);
+	return GameRounds;
 };
 void ResetScrean() {
-
+	system("cls");
+	system("color 0F");
 }
 void startGame() {
-
+	char playAgain = 'Y';
+	do {
+		ResetScrean();
+		stGameResults GameResults = playGame(ReadHowManyRounds());
+		showGameOverScrean();
+		ShowFinalGameResults(GameResults);
+		cout << endl << Tabs(3) << "Do you want to olay again? y/n";
+		cin >> playAgain;
+	} while (playAgain == 'y' || playAgain == 'Y');
 }
+
 int main()
 {
 	srand((unsigned)time(NULL));
+	startGame();
 }
